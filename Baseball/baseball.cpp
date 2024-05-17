50db1dc63f4497a1607db3162b125720f17d8057
jb0114.lee <jb0114.lee@samsung.com>
2024-05-17 13:56:27 +0900

    [feature] page 30

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -10,5 +10,10 @@ public:
 		if (input.length() != 3) {
 			throw length_error("string length should be 3");
 		}
+		for (char ch : input) {
+			if (ch < '0' || ch >'9') {
+				throw invalid_argument("input should consist of number");
+			}
+		}
 	}
 };
