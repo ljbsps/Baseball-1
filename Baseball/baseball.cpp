e89b5f89f5631b8bedd3ca5c4e5c569130fa0526
jb0114.lee <jb0114.lee@samsung.com>
2024-05-17 13:53:07 +0900

    [feature] page 27

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
 class Baseball {
 public:
-
+	string guess(const string input) {
+		throw length_error("string length should be 3");
+	}
 };
