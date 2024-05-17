8fb04046a3563e588e5063d6b4c3b80b4254701f
jb0114.lee <jb0114.lee@samsung.com>
2024-05-17 14:09:23 +0900

    [refactoring] page 42

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -6,19 +6,29 @@ using namespace std;
 
 class Baseball {
 public:
-	string guess(const string input) {
+	void guess(const string input) {
+		assertIllegalArgument(input);
+	}
+
+private:
+	void assertIllegalArgument(const std::string& input)
+	{
 		if (input.length() != 3) {
 			throw length_error("String length should be 3");
 		}
 		for (char ch : input) {
-			if (ch < '0' || ch >'9') {
-				throw invalid_argument("String should consist of number");
-			}
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("String should consist of number");
 		}
-		if (input[0] == input[1]
-			|| input[0] == input[2]
-			|| input[1] == input[2]) {
+		if (isDuplicatedNumber(input)) {
 			throw invalid_argument("String should not have same number");
 		}
+	}
+
+	bool isDuplicatedNumber(const string& input)
+	{
+		return input[0] == input[1]
+			|| input[0] == input[2]
+			|| input[1] == input[2];
 	}
 };
